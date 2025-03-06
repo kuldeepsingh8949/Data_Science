ae661eb86e07e54e0f57c3f6844d60a65858eff8
kuldeepsingh8949 <kuldeepraika64@gmail.com>
2025-03-07 00:50:23 +0530

    hello

--- Data_science/python/chat_application/sender.py
+++
@@ -1,9 +0,0 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-ip_add = "192.168.221.8"
-port = 1234
-complete = (ip_add, port)
-message =input("kya msg kroge ")
-encode_mgs=message.encode('ascii')
-s.sendto(encode_mgs,complete)
