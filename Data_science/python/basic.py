e7ebce3385e949ccca7a9b7927af93785a309e5d
Kuldeep Singh <146047783+kuldeepsingh8949@users.noreply.github.com>
2025-03-06 23:38:51 +0530

    Add files via upload

---
+++ Data_science/python/basic.py
@@ -0,0 +1,81 @@
+# print("hey arya")
+# a = 5
+# print(type(a))
+
+
+# #string methods
+# str  = "python is easy"
+# print(str.upper())
+# print(str.strip())
+# print(str.split())
+# print(str.replace("easy","hard"))
+# print(str.find("is"))
+
+# str2 = "hello"
+# str3 = "kese ho"
+# print(str2+str3)
+# print(str2+ " " +str3)
+
+
+# #tuple = unmutable data type
+# tuple = (1,2,3,"hello")
+# print(tuple)
+# print(type(tuple))
+
+# # operator
+# t = (1,2,3,4,6,7,0)
+# # print(t+tuple)
+# print(t*3)
+# print(t.count(7))
+# print(t.index(4))
+
+# #slicing
+# print(t[2:4])
+
+
+# # LIST = mutable data type
+# l = [1,2,3,4,5,6]
+# print(l)
+# print(type(l))
+# l[3]="hii"   # modifing
+# print(l)
+# print(l.append(4))  # add element
+# print(l)
+# l.insert(4,3)
+# print(l)
+# l.remove(3)
+# print(l)
+# l.pop()    # remove last element
+# print(l)
+# l.reverse()
+# print(l)
+
+# a = [1,28,43,35,678]
+# a.sort()
+# print(a)
+
+
+# a = {
+#     "rahul":"kumar",
+#     "rohit":"sharma",
+#     "rohan":"verma"
+# }
+# print(a)
+# print(type(a))
+# print(a.keys())
+# print(a.values())
+
+# print(print)
+
+# print(True+True+False)
+
+## Even Or Odd 
+
+# a = int(input("Enter the number")) 
+# if(a%2==0):
+#     print("even number")
+# else:
+#     print("odd")
+
+
+
